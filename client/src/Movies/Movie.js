5c3c683369cda35a4a099790de071882f9114373
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-07-16 17:03:33 -0700

    updated done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
+  const history = useHistory();
   const [movie, setMovie] = useState(null);
   const params = useParams();
 
@@ -33,6 +34,7 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick= {() => history.push(`/movies/update-movie/${movie.id}`)}>Update Movie Info</button>
     </div>
   );
 }
